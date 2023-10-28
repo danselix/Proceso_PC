bc906a425bbd69c2711e522fd50fb892afd08eac
Danselix95 <thecellomen@hotmail.com>
2023-10-28 00:55:38 -0500

    actualizando clases de js no. 5 y curso de DOM js

---
+++ Full-stack/Sergie_Code/curso_js/clase_5/scriptClase5.js
@@ -0,0 +1,26 @@
+// ARRAYS
+
+                 // 0             1            2 
+const array = ["curso html", "curso css", "curso JS"]
+
+// tambien se puede escribir asi:
+const ejemplo = [
+    "curso html", 
+    "curso css", 
+    "curso JS"
+]
+
+
+// Esta es una manera de inicializar un array vacio con los espacios definidos
+const array1 = new Array (5)
+
+array1[0] = "curso de HTML"
+array1[1] = "Curso de CSS"
+array1[2] = "Curso de JS" 
+
+document.getElementById('seleccionable1').innerHTML = array[0]
+document.getElementById('seleccionable2').innerHTML = array[1]
+document.getElementById('seleccionable3').innerHTML = array[2]
+
+
+
