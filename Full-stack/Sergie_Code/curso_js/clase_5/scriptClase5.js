5c48a1d4df8cdcd7f86350263f5f773d09e0235d
Danselix95 <thecellomen@hotmail.com>
2023-11-18 07:45:58 -0500

    actualizacion curso del DOM

--- Full-stack/Sergie_Code/curso_js/clase_5/scriptClase5.js
+++ Full-stack/Sergie_Code/curso_js/clase_5/scriptClase5.js
@@ -17,10 +17,22 @@ const array1 = new Array (5)
 array1[0] = "curso de HTML"
 array1[1] = "Curso de CSS"
 array1[2] = "Curso de JS" 
+ 
+/* 
+    Se puede colocar un array dentro de otro array:
+
+    array1[3] = [1,2,3,4,5]
+    
+    La diferencia entre array y objeto es la forma de la declaracion. el array se declara usando "[]" mientras que el objeto se declara usando "{}"
+
+      var array = [0,1,2,3]:
+      var objeto = {clave:"valor"};
+      
+*/
 
 document.getElementById('seleccionable1').innerHTML = array[0]
 document.getElementById('seleccionable2').innerHTML = array[1]
 document.getElementById('seleccionable3').innerHTML = array[2]
 
-
+console.log(array1);
 
