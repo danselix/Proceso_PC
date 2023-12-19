bd80fe9b082e0b5b2f7d3b1178c373d06c66650a
Danselix95 <thecellomen@hotmail.com>
2023-12-19 06:41:51 -0500

    actualizacion de Javascript DOM y Scripting(linux)

---
+++ Full-stack/JavaScript/manipulacion_del_dom/toppings_de_pizza/script1.js
@@ -0,0 +1,11 @@
+/* const titulo = document.getElementById('titulo');
+console.log(titulo) */
+
+// getElementByClassName()
+
+/* const toppings = document.getElementsByClassName('toppings')
+console.log(toppings[3]); */
+
+
+// getElementByTagName()
+
---
+++ Scripting/practica/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "estoy en el repositorio: "
+pwd
+
+echo "y ahora ire a home."
+cd ~
