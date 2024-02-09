1b593370921048426fa221dde4d7b9d1f541efc7
Danselix95 <thecellomen@hotmail.com>
2024-02-09 17:27:42 -0500

    elimininando curso olvidado de python

--- Full-stack/ProgramadorX_Python_desde_cero/condicionales/else-if.py
+++
@@ -1,8 +0,0 @@
-ingresoMensual = 10000
-
-if ingresoMensual > 10000:
-    print("Estoy excelente economicamente")
-elif ingresoMensual > 5000:
-    print("Siempre estoy agradecido por lo que tengo")
-else:
-    print("siempre puedes trabajar para ganar algo de dinero no?")
