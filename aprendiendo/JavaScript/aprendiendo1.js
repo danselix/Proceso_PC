2fef4e2dc966b6ca3bc6209c941fe6a92690cc37
Danselix95 <thecellomen@hotmail.com>
2024-03-31 22:35:44 -0500

    actualizacion aprendiendo javascript y EjerciciosWebHenry

--- aprendiendo/JavaScript/aprendiendo1.js
+++ aprendiendo/JavaScript/aprendiendo1.js
@@ -1,26 +1,31 @@
 const usuario = {
-    nombre: "",
-    edad: "",
-    estilo: "",
-    arma: "",
-    escudo: ""
-  };
-  
-  usuario.nombre = "Danselix95";
-  usuario.edad = 27;
-  usuario.estilo = {
-    nombre:"mago",
-    energia: "10000",
-    elemento: "aire",
-    experiencia: "10000"
+  nombre: "",
+  edad: "",
+  estilo: "",
+  arma: "",
+  escudo: "",
 };
-  usuario.arma = {
-    nombre: "telekinesis",
-    poder: 10,
-    tipo: "mental",
-    alcance: "medio"
+
+usuario.nombre = "Danselix95";
+usuario.edad = 27;
+usuario.estilo = {
+  nombre: "mago",
+  energia: "10000",
+  elemento: "aire",
+  experiencia: "10000",
 };
-  usuario.escudo = "libro mágico";
-  
-  console.log(usuario.nombre);
-  console.log(usuario.estilo.elemento)
+usuario.arma = {
+  nombre: "telekinesis",
+  poder: 10,
+  tipo: "mental",
+  alcance: "medio",
+};
+usuario.escudo = "libro mágico";
+
+/* console.log(usuario.nombre);
+console.log(usuario.estilo.elemento);
+console.log(usuario.estilo); */
+
+usuario.arma.poder = 100; // cambiandole valor a una propiedad agregada a la calse usuario
+
+console.log(usuario);
