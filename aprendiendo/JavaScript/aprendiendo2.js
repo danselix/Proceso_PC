2ceaf05158eae8ff792cbbab1a74f2cefe37c327
thecellomen@hotmail.com <thecellomen@hotmail.com>
2024-08-08 21:37:57 -0500

    actualizando el aprendizaje de javascript-2

---
+++ aprendiendo/JavaScript/aprendiendo2.js
@@ -0,0 +1,10 @@
+console.log(`la multiplicacion de 10 * 3 es ${10 * 3}`); // esta es la forma de hacer interpolacion de codigo
+                                                         // importante tener en cuenta las comillas invertidas para que ejecute el codigo correctamente
+
+console.log(`10 * 3 es: ${10 * 3}`);
+// ejemplo de chatgpt para solucionar lo de las comillas invertidas (altgr + "}")
+
+
+
+
+
